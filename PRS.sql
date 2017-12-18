a2aeb1426a5e527660f4fb81c9371647cf36df0b
mathieu auclair <mathiass12@hotmail.com>
2017-12-18 00:47:50 -0500

    adding trigger

--- PRS.sql
+++ PRS.sql
@@ -11,14 +11,16 @@ IF EXISTS
 GO
 CREATE PROCEDURE psResultatsJoueurs(@noTournoi INT, @noJoueur INT, @anneeTournoi INT) AS
   BEGIN
-DECLARE @NbPoints INT, @Gains MONEY
-	SELECT dbo.tblResultats.nbPoints,dbo.tblResultats.gains
-      FROM dbo.tblResultats
-      WHERE anneeTournoi = @anneeTournoi AND noTournoi = @noTournoi AND noJoueur = @noJoueur
-	  RETURN
-	  END
+    DECLARE @NbPoints INT, @Gains MONEY
+    SELECT
+      dbo.tblResultats.nbPoints,
+      dbo.tblResultats.gains
+    FROM dbo.tblResultats
+    WHERE anneeTournoi = @anneeTournoi AND noTournoi = @noTournoi AND noJoueur = @noJoueur
+    RETURN
+  END
 
-	  EXEC psResultatsJoueurs @noTournoi=1,@noJoueur=1,@anneeTournoi=2008
+EXEC psResultatsJoueurs @noTournoi = 1, @noJoueur = 1, @anneeTournoi = 2008
 
 -----------#2----------
 GO
@@ -33,11 +35,29 @@ CREATE PROCEDURE psZoneGeographique(@unCodeNationalite CHAR(3)) AS
   BEGIN
     DECLARE @CodeZone INT
     SELECT dbo.tblNationalite.codeZone
-            FROM dbo.tblNationalite
-            WHERE dbo.tblNationalite.codeNationalite = @unCodeNationalite
-			RETURN @CodeZone
+    FROM dbo.tblNationalite
+    WHERE dbo.tblNationalite.codeNationalite = @unCodeNationalite
+    RETURN @CodeZone
   END
 
-EXEC psZoneGeographique @unCodeNationalite = 'CZE'
+EXEC pszonegeographique @uncodenationalite = 'cze'
 
 ----------#3------------------
+GO
+IF EXISTS
+(SELECT *
+ FROM dbo.sysobjects
+ WHERE id = object_id(N'dbo.psNomTableJoueur')
+       AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
+  DROP PROCEDURE dbo.psNomTableJoueur
+GO
+CREATE PROCEDURE psNomTableJoueur(@Code CHAR(3))
+AS
+  BEGIN
+    DECLARE @CodeZone INTEGER
+    EXECUTE psZoneGeographique @unCodeNationalite = @Code
+    SELECT dbo.tblNationalite.nomPays
+    FROM tblNationalite
+    WHERE tblNationalite.codeZone = @CodeZone
+  END
+EXEC pszonegeographique @uncodenationalite = 'CZE'
---
+++ TRG.sql
@@ -0,0 +1,78 @@
+USE ProjetBD2017
+GO
+DROP TRIGGER IF EXISTS dbo.AjoutTournoiCommanditaire
+GO
+CREATE TRIGGER dbo.AjoutTournoiCommanditaire
+  ON tblTournoi
+  AFTER INSERT
+AS
+  BEGIN
+    SET NOCOUNT ON;
+      BEGIN
+        INSERT INTO dbo.tblTournoiCommandite
+          VALUES (1,(SELECT inserted.noTournoi FROM inserted))
+      END
+  END
+GO
+INSERT INTO dbo.tblTournoi
+VALUES ('pickle contest', 'Jonquiere', 'Canada', 100000000)
+GO
+SELECT * FROM dbo.tblTournoi
+GO
+DROP TRIGGER IF EXISTS dbo.AjoutCommanditaireTournoi
+GO
+CREATE TRIGGER dbo.AjoutCommanditaireTournoi
+  ON dbo.tblCommanditaires
+  AFTER INSERT
+  AS
+  BEGIN
+    SET NOCOUNT ON;
+    BEGIN
+      INSERT INTO dbo.tblTournoiCommandite
+        VALUES ((SELECT inserted.noCommanditaire FROM inserted), 1)
+    END
+  END
+GO
+INSERT INTO dbo.tblCommanditaires
+    VALUES (7, 'SolutionsTLM')
+GO
+SELECT * FROM tblCommanditaires
+GO
+DROP TRIGGER IF EXISTS AjoutVueListeJoueurs
+GO
+CREATE TRIGGER AjoutVueListeJoueurs
+  ON dbo.VueListeJoueurs
+  INSTEAD OF INSERT
+  AS
+  BEGIN
+    SET NOCOUNT ON;
+    BEGIN
+      DECLARE @region INT
+
+      IF(@region = 1)
+        INSERT INTO dbo.tblJoueursAfrique
+          VALUES (SELECT * FROM inserted)
+      IF(@region = 2)
+        INSERT INTO dbo.tblJoueursAmeriqueNord
+          VALUES (SELECT * FROM inserted)
+      IF(@region = 3)
+        INSERT INTO dbo.tblJoueursAmeriqueSud
+          VALUES (SELECT * FROM inserted)
+      IF(@region = 4)
+        INSERT INTO dbo.tblJoueursAsie
+          VALUES (SELECT * FROM inserted)
+      IF(@region = 5)
+        INSERT INTO dbo.tblJoueursEurope
+          VALUES (SELECT * FROM inserted)
+      IF(@region = 6)
+        INSERT INTO dbo.tblJoueursOceanie
+          VALUES (SELECT * FROM inserted)
+      ROLLBACK TRANSACTION;
+      RETURN
+    END
+  END
+GO
+INSERT INTO dbo.VueListeJoueurs
+    VALUES (100, 'MisterMoist', 'Stun', 1000, 'FRA')
+GO
+SELECT * FROM dbo.tblJoueursEurope
