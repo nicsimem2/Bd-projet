fc1154c6d4235593a95ed15d80db864ea31fb1be
mathieu auclair <mathiass12@hotmail.com>
2017-12-17 22:49:56 -0500

    fixing prs

--- PRS.sql
+++ PRS.sql
@@ -9,16 +9,29 @@ IF EXISTS
        AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
   DROP PROCEDURE dbo.psResultatsJoueurs
 GO
-CREATE PROCEDURE psResultatsJoueurs(@noTournoi int,@noJoueur int,@anneeTournoi int) AS
-BEGIN
-DECLARE @NbPoints int;
-DECLARE @Gains money;
-SELECT @NbPoints= nbPoints,@Gains=gains
+CREATE PROCEDURE psResultatsJoueurs(@noTournoi INT, @noJoueur INT, @anneeTournoi INT) AS
+  BEGIN
+    DECLARE
+    @NbPoints INT,
+    @Gains MONEY
+    RETURN (
+      SELECT
+        @NbPoints = dbo.tblResultats.nbPoints,
+        @Gains = dbo.tblResultats.gains
+      FROM dbo.tblResultats
+      WHERE anneeTournoi = @anneeTournoi
+      AND noTournoi = @noTournoi
+      AND noJoueur = @noJoueur)
+
+  END
+EXEC psResultatsJoueurs @noTournoi = 1, @noJoueur = 1, @anneeTournoi = 2008
+
+
+SELECT
+  dbo.tblResultats.nbPoints,
+  dbo.tblResultats.gains
 FROM dbo.tblResultats
-WHERE  @anneeTournoi=anneeTournoi AND @noTournoi = noTournoi AND @noJoueur = noJoueur
-RETURN SELECT @Gains,@NbPoints;
-PRINT(@Gains+' '+@NbPoints)
-END
+WHERE anneeTournoi AND @noTournoi = noTournoi AND @noJoueur = noJoueur
 
 -----------#2----------
 GO
@@ -29,14 +42,12 @@ IF EXISTS
        AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
   DROP PROCEDURE dbo.psZoneGeographique
 GO
+CREATE PROCEDURE psZoneGeographique(@unCodeNationalite CHAR(3)) AS
+  BEGIN
+    DECLARE @CodeZone INT
+    RETURN (SELECT dbo.tblNationalite.codeZone
+            FROM dbo.tblNationalite
+            WHERE dbo.tblNationalite.codeNationalite = @unCodeNationalite)
+  END
 
-CREATE PROCEDURE psZoneGeographique(@unCodeNationalite char(3)) AS
-BEGIN
-DECLARE @CodeZone int
-SELECT codeZone = @CodeZone
-FROM tblNationalite
-WHERE  codeNationalite = @unCodeNationalite
-RETURN @CodeZone
-END
-
-EXEC psZoneGeographique @unCodeNationalite='CZE'
+EXEC psZoneGeographique @unCodeNationalite = 'CZE'
