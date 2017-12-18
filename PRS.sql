abc1d18c35dfd91f9e14c83446b3b1ffb498789f
nicsimem2 <nicolas_1_@hotmail.fr>
2017-12-17 21:37:43 -0500

    WIP 2

--- PRS.sql
+++ PRS.sql
@@ -1,5 +1,6 @@
 USE ProjetBD2017
 
+------#1-----------
 GO
 IF EXISTS
 (SELECT *
@@ -19,4 +20,23 @@ RETURN SELECT @Gains,@NbPoints;
 PRINT(@Gains+' '+@NbPoints)
 END
 
-exec psResultatsJoueurs @noTournoi=1 ,@noJoueur=1,@anneeTournoi=2008
+-----------#2----------
+GO
+IF EXISTS
+(SELECT *
+ FROM dbo.sysobjects
+ WHERE id = object_id(N'dbo.psZoneGeographique')
+       AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
+  DROP PROCEDURE dbo.psZoneGeographique
+GO
+
+CREATE PROCEDURE psZoneGeographique(@unCodeNationalite char(3)) AS
+BEGIN
+DECLARE @CodeZone int
+SELECT codeZone = @CodeZone
+FROM tblNationalite
+WHERE  codeNationalite = @unCodeNationalite
+RETURN @CodeZone
+END
+
+EXEC psZoneGeographique @unCodeNationalite='CZE'
