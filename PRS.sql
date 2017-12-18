bda1e0477d9afb873b29e738d593d78bdb8a5e24
mathieu auclair <mathiass12@hotmail.com>
2017-12-18 04:59:08 -0500

    add trigger + fix PRS

--- PRS.sql
+++ PRS.sql
@@ -47,30 +47,52 @@ DECLARE @output INT
 EXEC psZoneGeographique @unCodeNationalite = 'CZE', @unCodeZone = @output OUTPUT
 
 ----------#3------------------
+DROP PROCEDURE IF EXISTS psNomTableJoueur
 GO
-IF EXISTS
-(SELECT *
- FROM dbo.sysobjects
- WHERE id = object_id(N'dbo.psNomTableJoueur')
-       AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
-  DROP PROCEDURE dbo.psNomTableJoueur
-GO
-
 CREATE PROCEDURE psNomTableJoueur
-    @CodeNationalite CHAR(3)
+    @codeNationalite CHAR(3),
+    @nomTableJoueur  VARCHAR(30) OUTPUT
 AS
   BEGIN
-    DECLARE @output INT
-    EXEC psZoneGeographique @unCodeNationalite = 'CZE', @unCodeZone = @output OUTPUT
-    SELECT @output
-    SELECT nomZone
-    FROM tblZoneGeographique
-    WHERE codeZone = 5
-    RETURN
+    DECLARE @zone INT
+    EXEC psZoneGeographique 'FRA', @zone OUTPUT
+    PRINT (@zone)
+    IF EXISTS(SELECT *
+              FROM tblNationalite
+              WHERE dbo.tblNationalite.codeNationalite = @codeNationalite)
+      BEGIN
+        EXEC psZoneGeographique @codeNationalite, @zone OUTPUT
+        SELECT @nomTableJoueur = CASE @zone
+                                 WHEN 1
+                                   THEN 'tblJoueursAfrique'
+                                 WHEN 2
+                                   THEN 'tblJoueursNA'
+                                 WHEN 3
+                                   THEN 'tblJoueursSA'
+                                 WHEN 4
+                                   THEN 'tblJoueursAsie'
+                                 WHEN 5
+                                   THEN 'tblJoueursEU'
+                                 WHEN 6
+                                   THEN 'tblJoueursOCE'
+                                 END
+      END
+    ELSE BEGIN
+      PRINT 'Code de nationalite incorrecte'
+    END
   END
-DECLARE @output2 INT
-EXEC psNomTableJoueur @unCodeNationalite = 'CZE'
+GO
+PRINT ''
+PRINT 'Procédure 3'
+DECLARE @tableRep VARCHAR(30)
+EXECUTE psNomTableJoueur 'FRA', @tableRep OUTPUT
+PRINT @tableRep
+
 --------#4-------------------
+
+
+
+
 --------#5-------------------
 GO
 IF EXISTS
@@ -82,7 +104,7 @@ IF EXISTS
 GO
 CREATE PROCEDURE psCalculGains(@codeResultat INT, @noTournoi INT) AS
   BEGIN
-SELECT DISTINCT(bourseTournoi*txBourse) AS 'Gain Mon�taire'
+SELECT DISTINCT(bourseTournoi*txBourse) AS 'Gain Monetaire'
 FROM tblTournoi AS AA
 JOIN tblResultats AS BB
 ON aa.noTournoi=BB.noTournoi
@@ -125,45 +147,4 @@ GO
 CREATE PROCEDURE psAjoutResultat(@noJoueur INT,@noTournoi INT,@codeResultat INT,@anneeTournoi INT) AS
 
 
--- TAS DE MARDE A VICTOR
-DROP PROCEDURE IF EXISTS psNomTableJoueur
-GO
-CREATE PROCEDURE psNomTableJoueur
-    @codeNationalite CHAR(3),
-    @nomTableJoueur  VARCHAR(30) OUTPUT
-AS
-  BEGIN
-    DECLARE @zone INT
-    EXEC psZoneGeographique 'FRA', @zone OUTPUT
-    PRINT (@zone)
-    IF EXISTS(SELECT *
-              FROM tblNationalite
-              WHERE dbo.tblNationalite.codeNationalite = @codeNationalite)
-      BEGIN
-        EXEC psZoneGeographique @codeNationalite, @zone OUTPUT
-        SELECT @nomTableJoueur = CASE @zone
-                                 WHEN 1
-                                   THEN 'tblJoueursAfrique'
-                                 WHEN 2
-                                   THEN 'tblJoueursNA'
-                                 WHEN 3
-                                   THEN 'tblJoueursSA'
-                                 WHEN 4
-                                   THEN 'tblJoueursAsie'
-                                 WHEN 5
-                                   THEN 'tblJoueursEU'
-                                 WHEN 6
-                                   THEN 'tblJoueursOCE'
-                                 END
-      END
-    ELSE BEGIN
-      PRINT 'Code de nationalite incorrecte'
-    END
-  END
-GO
-PRINT ''
-PRINT 'Procédure 3'
-DECLARE @tableRep VARCHAR(30)
-EXECUTE psNomTableJoueur 'FRA', @tableRep OUTPUT
-PRINT @tableRep
 
