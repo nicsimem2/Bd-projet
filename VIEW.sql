3726d76bdf64bc69f5c65675e079df06f9c9f720
mathieu auclair <mathiass12@hotmail.com>
2017-12-17 20:41:14 -0500

    adding views

--- VIEW.sql
+++ VIEW.sql
@@ -20,22 +20,53 @@ CREATE VIEW dbo.VueListeJoueurs
     SELECT *
     FROM dbo.tblJoueursOceanie
 GO
-SELECT * FROM dbo.VueListeJoueurs
+SELECT *
+FROM dbo.VueListeJoueurs
 GO
 DROP VIEW IF EXISTS VueClassementJoueurs
 GO
 CREATE VIEW VueClassementJoueurs
   AS
-  SELECT dbo.VueListeJoueurs.nomJoueur,
-  dbo.VueListeJoueurs.prenomJoueur,
-  dbo.tblResultats.gains,
-  dbo.tblResultats.nbPoints
-  FROM dbo.VueListeJoueurs
-  LEFT JOIN dbo.tblResultats
-  ON dbo.VueListeJoueurs.noJoueur = dbo.tblResultats.noJoueur
+    SELECT
+      dbo.VueListeJoueurs.nomJoueur,
+      dbo.VueListeJoueurs.prenomJoueur,
+      dbo.tblResultats.gains,
+      dbo.tblResultats.nbPoints
+    FROM dbo.VueListeJoueurs
+      LEFT JOIN dbo.tblResultats
+        ON dbo.VueListeJoueurs.noJoueur = dbo.tblResultats.noJoueur
 GO
-SELECT * FROM dbo.VueClassementJoueurs
+SELECT *
+FROM dbo.VueClassementJoueurs
 GO
-DROP VIEW IF EXISTS  VueNbJoueursParContinent
-AS
-SELECT
+DROP VIEW IF EXISTS VueNbJoueursParContinent
+GO
+CREATE VIEW VueNbJoueursParContinent
+  AS
+    SELECT
+      COUNT(dbo.VueListeJoueurs.noJoueur) AS COUNT,
+      dbo.tblNationalite.nomPays
+    FROM dbo.VueListeJoueurs
+      JOIN dbo.tblNationalite
+      JOIN dbo.tblZoneGeographique
+        ON dbo.tblZoneGeographique.codeZone = dbo.tblNationalite.codeZone
+        ON dbo.tblNationalite.codeNationalite = dbo.VueListeJoueurs.codeNationalite
+    GROUP BY dbo.tblNationalite.nomPays
+GO
+SELECT *
+FROM VueNbJoueursParContinent
+GO
+DROP VIEW IF EXISTS VueNbNationaliteParContinent
+GO
+CREATE VIEW dbo.VueNbNationaliteParContinent
+  AS
+    SELECT
+      COUNT(dbo.tblNationalite.codeZone) AS COUNT,
+      dbo.tblZoneGeographique.codeZone,
+      dbo.tblZoneGeographique.nomZone
+    FROM dbo.tblNationalite
+      JOIN dbo.tblZoneGeographique
+        ON dbo.tblNationalite.codeZone = dbo.tblZoneGeographique.codeZone
+    GROUP BY dbo.tblZoneGeographique.nomZone, dbo.tblZoneGeographique.codeZone
+GO
+SELECT * FROM dbo.VueNbNationaliteParContinent
